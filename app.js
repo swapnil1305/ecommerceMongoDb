a2e9b1596d0cf9b7083427350d2161aeafa1d207
swapnil1305 <swapnil.satpute132@gmail.com>
2023-04-04 22:41:57 +0530

    user table

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById("642b0295bd95d70e61ad1ab2")
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("642c580c7618e989bc078789")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,18 @@ mongoose
   'mongodb+srv://swapnil:swapnil7@cluster0.sbzopou.mongodb.net/shop?retryWrites=true&w=majority'
   )
 .then( result => {
+  User.findOne().then(user => {
+    if (!user) {
+      const user = new User({
+        name: 'rock',
+        email: 'rock@test.com',
+        cart: {
+          items:[]
+        }
+      });
+      user.save()
+    }
+  })
  app.listen(3000);
 })
 .catch(err => {
